5701407ad8439587e183417e1e8674852d85f751
TasnimMahfuz <mahfuznafis1@gmail.com>
2023-03-15 15:51:37 +0600

    unfinished code with changed grammar

--- contextFreeGrammar.cpp
+++ contextFreeGrammar.cpp
@@ -4,6 +4,7 @@ using namespace std;
 bool isPossible(string input);
 int prefixZeroCount(string input);
 void formatIt();
+int countSubZero(string input);
 
 
 int main()
@@ -13,7 +14,7 @@ int main()
     cin >> inputWord;
 
 
-    vector <string> transitionS = {"A1B"};
+    vector <string> transitionS = {"A1B00"};
     vector <string> transitionA = {"0A","epsilon"};
     vector <string> transitionB = {"0B","1B","epsilon"};
 
@@ -27,6 +28,7 @@ int main()
         formatIt();
 
         int preZeroCnt = prefixZeroCount(inputWord);
+        int subZeroCnt = 0;
 
         string outputTemp = "";
 
@@ -80,35 +82,119 @@ int main()
 
         formatIt();
 
+
+
         cout<<"\t\t\t"<<inputWord <<endl;
 
         formatIt();
 
         cout <<"\t\t\tWe have arrived at the destination!\n\n";
 
+        return 0;
 
-    }
-    return 0;
+        }
+
 
 }
 
+        /*
+        cout <<"Now let us see the rightmost derivation: \n\n\n";
+        transitionS.push_back("A1");
+
+        cout<<"\t\t\t S\n";
+        formatIt();
+        cout<<"\t\t\t A1B\n";
+        formatIt();
+
+        idx = 0;
+
+        for( ; idx < inputWord.size() and inputWord[idx] != '1'; idx++);
+
+        idx++;
+
+        while(idx < inputWord.size())
+        {
+
+            if(inputWord[idx] == '1')
+            {
+
+                transitionS[2] += "1";
+            }else
+            {
+                transitionS[2] += "0";
+            }
+
+            cout <<"\t\t\t" << transitionS[2]+"B"<<endl;
+
+            formatIt();
+
+            idx++;
+        }
+
+
+
+        cout << "\t\t\t"<<transitionS[2] + transitionB[2]<<endl;
+
+        formatIt();
+
+         cout << "\t\t\t"<<transitionS[2] <<endl;
+
+        formatIt();
+
+
+        outputTemp = "";
+        for(int i = 1; i <= preZeroCnt; i++)
+        {
+
+            outputTemp += "0";
+            cout << "\t\t\t"<<outputTemp + transitionS[2] <<endl;
+            formatIt();
+
+
+        }
+
+        cout<<"\t\t\t"<<inputWord <<endl;
+
+        formatIt();
+        cout <<"\t\t\tWe have arrived at the destination!\n\n";
+
+
+    }*/
+
+    //return 0;
+
+//}
+
 bool isPossible(string input)
 {
-    int oneCnt = 0;
     for(auto chars: input)
     {
         if(chars != '1' and chars != '0')
         {
             return false;
         }
-        if(chars == '1')
-            oneCnt++;
+
     }
 
-    if(oneCnt >= 1)
-        return true;
-    else
-        return false;
+    int zeroSuf = 0;
+    bool f = false;
+
+    for(auto chars: input)
+    {
+
+
+        if(chars == '1')
+            f = true;
+
+    }
+
+    if(input.size() >=2)
+    {
+        if(f == true and input[input.size() - 1] == '0' and input[input.size() - 2] == '0')
+            return true;
+    }
+
+    return false;
 
 }
 
@@ -132,5 +218,22 @@ void formatIt()
 {
     for(int i = 0; i < 3; i++)
         cout <<"\t\t\t |\n";
+}
+
+int countSubZero(string input)
+{
+    int idx = 0;
+
+    for( ; idx < input.size() and input[idx] != '1'; idx++);
+
+    idx++;
+
+    int ans = 0;
+
+    for( ; idx < input.size(); idx++)
+    if(input[idx] == '0')
+        ans++;
+
+    return ans;
 }
 
